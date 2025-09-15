0f154aec742cc04225484c28ddd7863017594e58
emieli <emil.eliason@gmail.com>
2025-09-15 14:02:57 +0200

    Ufo: Remove ugly foldcolumn

--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -3,8 +3,7 @@ return { -- Help with code folding
     event = "BufRead",
     dependencies = { "kevinhwang91/promise-async" },
     config = function()
-        -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-        vim.o.foldcolumn = '1' -- '0' is not bad
+        vim.o.foldcolumn = '0' -- Remove ugly foldcolumn
         vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
