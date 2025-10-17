cec4efab2b65f48b3803a4a090c58b48451525b6
emileli <emil.eliasson@tele2.com>
2025-10-17 21:16:37 +0200

    Code Comments: Make '# ' default comment prefix

--- .config/nvim/lua/config/code_comments.lua
+++ .config/nvim/lua/config/code_comments.lua
@@ -1,13 +1,12 @@
 -- https://slar.se/comment-and-uncomment-code-in-neovim.html
 local non_c_line_comments_by_filetype = {
-	go = "//",
-	lua = "--",
-	python = "#",
-	sql = "--",
+	go = "// ",
+	lua = "-- ",
+	sql = "-- ",
 }
 
 local function comment_out(opts)
-	local line_comment = non_c_line_comments_by_filetype[vim.bo.filetype] or "//"
+	local line_comment = non_c_line_comments_by_filetype[vim.bo.filetype] or "# "
 	local start = math.min(opts.line1, opts.line2)
 	local finish = math.max(opts.line1, opts.line2)
 
@@ -16,7 +15,7 @@ local function comment_out(opts)
 end
 
 local function uncomment(opts)
-	local line_comment = non_c_line_comments_by_filetype[vim.bo.filetype] or "//"
+	local line_comment = non_c_line_comments_by_filetype[vim.bo.filetype] or "# "
 	local start = math.min(opts.line1, opts.line2)
 	local finish = math.max(opts.line1, opts.line2)
 
