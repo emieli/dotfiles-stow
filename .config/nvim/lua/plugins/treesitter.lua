d4f0ef8f657892ab04464aaf91f9062b3be56a88
Emil Boklund <emel02@Emils-MacBook-Pro.local>
2026-03-01 21:20:36 +0100

    Use treesitter master, without it catppuccin highlighting sucks

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,8 +3,9 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
+		branch = "master",
 		config = function()
-			local config = require("nvim-treesitter.config")
+			local config = require("nvim-treesitter.configs")
 			config.setup({
 				auto_install = true,
 				highlight = { enable = true },
